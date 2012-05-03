81730c1c495f2b0118548ee0809d1ad6cea07649
Strangerke <strangerke@scummvm.org>
2012-05-03 19:35:53 +0200

    ROBIN: Fix some uninitialized variables reported by Valgrind (Thanks Hkz for reporting)

--- engines/robin/script.cpp
+++ engines/robin/script.cpp
@@ -2219,8 +2219,10 @@ void RobinScript::OC_sub17E07() {
 	_currScript->readUint16LE();
 
 }
+
 void RobinScript::OC_sub17E15() {
 	debugC(1, kDebugScript, "OC_sub17E15()");
+
 	if ((_word1881B & 0xFF) == 0xFF) {
 		OC_sub17DB9();
 		return;
@@ -2299,6 +2301,7 @@ void RobinScript::OC_resetByte1714E() {
 void RobinScript::OC_deleteSavegameAndQuit() {
 	warning("OC_deleteSavegameAndQuit");
 }
+
 void RobinScript::OC_incByte16F04() {
 	debugC(1, kDebugScript, "OC_incByte16F04()");
 
@@ -2316,6 +2319,7 @@ void RobinScript::OC_sub17BA5() {
 
 void RobinScript::OC_setByte18823() {
 	debugC(1, kDebugScript, "OC_setByte18823()");
+
 	byte *tmpArr = getCharacterVariablePtr();
 	_byte18823 = *tmpArr;
 }
@@ -2408,6 +2412,7 @@ void RobinScript::OC_sub17C0E() {
 
 void RobinScript::OC_sub17C55() {
 	debugC(1, kDebugScript, "OC_sub17C55()");
+
 	int var1 = getValue1();
 	int var2 = getValue1();
 
