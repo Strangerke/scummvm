282cde168c78d78cbead8d6ec1c0154f2773e3dc
Walter van Niftrik <walter@scummvm.org>
2012-10-07 17:25:52 +0200

    NANCY: Add resource decompressor

--- engines/nancy/console.cpp
+++ engines/nancy/console.cpp
@@ -20,15 +20,88 @@
  *
  */
 
+#include "common/system.h"
+#include "graphics/surface.h"
 #include "nancy/console.h"
 #include "nancy/nancy.h"
+#include "nancy/resource.h"
 
 namespace Nancy {
 
 NancyConsole::NancyConsole(NancyEngine *vm) : GUI::Debugger(), _vm(vm) {
+	DCmd_Register("res_hexdump", WRAP_METHOD(NancyConsole, Cmd_resHexDump));
+	DCmd_Register("res_list", WRAP_METHOD(NancyConsole, Cmd_resList));
+	DCmd_Register("res_info", WRAP_METHOD(NancyConsole, Cmd_resInfo));
+	DCmd_Register("res_show_image", WRAP_METHOD(NancyConsole, Cmd_resShowImage));
 }
 
 NancyConsole::~NancyConsole() {
+}
+
+bool NancyConsole::Cmd_resHexDump(int argc, const char **argv) {
+	if (argc != 2) {
+		DebugPrintf("Dumps the specified resource to standard output\n");
+		DebugPrintf("Usage: %s <resource name>\n", argv[0]);
+		return true;
+	}
+
+	uint size;
+	byte *buf = _vm->_res->loadResource(argv[1], size);
+	if (!buf) {
+		DebugPrintf("Failed to load resource '%s'", argv[1]);
+		return true;
+	}
+
+	Common::hexdump(buf, size);
+	return true;
+}
+
+bool NancyConsole::Cmd_resList(int argc, const char **argv) {
+	if (argc != 1) {
+		DebugPrintf("List all resources\n");
+		DebugPrintf("Usage: %s\n", argv[0]);
+		return true;
+	}
+
+	Common::Array<Common::String> list;
+	_vm->_res->listResources(list);
+	for (uint i = 0; i < list.size(); i++) {
+		DebugPrintf("%-10s", list[i].c_str());
+		if ((i % 8) == 7 && i + 1 != list.size())
+			DebugPrintf("\n");
+	}
+
+	DebugPrintf("\n");
+
+	return true;
+}
+
+bool NancyConsole::Cmd_resInfo(int argc, const char **argv) {
+	if (argc != 2) {
+		DebugPrintf("Prints information about a resource\n");
+		DebugPrintf("Usage: %s <resource name>\n", argv[0]);
+		return true;
+	}
+
+	DebugPrintf("%s", _vm->_res->getResourceDesc(argv[1]).c_str());
+	return true;
+}
+
+bool NancyConsole::Cmd_resShowImage(int argc, const char **argv) {
+	if (argc != 2) {
+		DebugPrintf("Draws an image on the screen\n");
+		DebugPrintf("Usage: %s <resource name>\n", argv[0]);
+		return true;
+	}
+
+	Graphics::Surface surf;
+	if (_vm->_res->loadImage(argv[1], surf)) {
+		_vm->_system->fillScreen(0);
+		_vm->_system->copyRectToScreen(surf.pixels, surf.pitch, 0, 0, surf.w, surf.h);
+		surf.free();
+	} else
+		DebugPrintf("Failed to load image\n");
+	return true;
 }
 
 } // End of namespace Nancy
--- engines/nancy/module.mk
+++ engines/nancy/module.mk
@@ -3,7 +3,8 @@ MODULE := engines/nancy
 MODULE_OBJS = \
   console.o \
   nancy.o \
-  detection.o
+  detection.o \
+  resource.o
 
 # This module can be built as a plugin
 ifdef BUILD_PLUGINS
